48a6ab54817a2d487b548d892be55c9d5c611275
Isaac <hawkins.isaac@gmail.com>
2018-12-14 12:59:16 -0600

    start configuration of graphql and apollo client

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
 import registerServiceWorker from './registerServiceWorker';
 import App from './App';
 
 import './style.css';
+
+// https://www.robinwieruch.de/react-graphql-apollo-tutorial
+
+const GITHUB_BASE_URL = 'https://api.github.com/graphql';
+
+const httpLink = new HttpLink({
+  uri: GITHUB_BASE_URL,
+  headers: {
+    authorization: `Bearer ${process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN}`
+  }
+});
+
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache
+});
 
 ReactDOM.render(
   <App />,
